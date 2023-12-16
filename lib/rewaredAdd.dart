b8e7d6a66af5d6b0b65213515fc2e81bf995679b
Fahim Shahariar Fahad <120266096+fahadAD@users.noreply.github.com>
2023-12-16 11:46:39 +0600

    Update rewaredAdd.dart

--- lib/rewaredAdd.dart
+++ lib/rewaredAdd.dart
@@ -20,6 +20,25 @@ class _RewardedADS_PageState extends State<RewardedADS_Page> {
         rewardedAdLoadCallback: RewardedAdLoadCallback(
             onAdLoaded: (ad) {
               _rewardedAd = ad;
+
+ad.fullScreenContentCallback = FullScreenContentCallback(
+                // Called when the ad showed the full screen content.
+                  onAdShowedFullScreenContent: (ad) {},
+                  // Called when an impression occurs on the ad.
+                  onAdImpression: (ad) {},
+                  // Called when the ad failed to show full screen content.
+                  onAdFailedToShowFullScreenContent: (ad, err) {
+                    // Dispose the ad here to free resources.
+                    ad.dispose();
+                  },
+                  // Called when the ad dismissed full screen content.
+                  onAdDismissedFullScreenContent: (ad) {
+                    // Dispose the ad here to free resources.
+                    ad.dispose();
+                  },
+                  // Called when a click is recorded for an ad.
+                  onAdClicked: (ad) {});
+              
             },
             onAdFailedToLoad: (error) {
               debugPrint('RewardedAd failed to load: $error');
